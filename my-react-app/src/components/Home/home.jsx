a55496e547f605675b11f02776309061530803b4
dinh van binh <dinhvanbinh2310@gmail.com>
2024-12-04 10:24:19 +0700

    new feature

--- my-react-app/src/components/Home/home.jsx
+++ my-react-app/src/components/Home/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Slider from './sliderHome';
+import RecipeReviewCard from './ImgCard';
 
 const Home = () => {
     return (
-        <div>
+        <div style={{ alignItems: 'center', justifyContent: 'center', flexDirection: 'column', display: 'flex' }}>
             <h1>Home</h1>
+            <RecipeReviewCard />
             <Slider />
         </div>
     )
